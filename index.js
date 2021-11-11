617af0fc13901445782d087f8187025ca52d2da0
mamyh <likhmaon@gmail.com>
2021-11-11 23:25:54 +0600

    initialization

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+
+
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+//middleware 
+app.use(cors());
+app.use(express.json());
+
+//connectino to the mongodb database 
+const uri = `mongodb+srv://${process.env.CAR_DB_USER}:${process.env.CAR_DB_PASSWORD}@cluster0.lt029.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('car-sale');
+        const usersCollection = database.collection('users');
+        const productsCollection = database.collection('products');
+
+        //users api
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+        app.put('/users', async (req, res) => {
+
+        })
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('hello car saller');
+});
+
+app.listen(port, () => {
+    console.log('listening to the port ', port);
+})
+
